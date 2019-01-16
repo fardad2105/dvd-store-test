3b47a8c3169e56d41d9bdeecebef66e5690abfd4
fardad <fardadyadegare@yahoo.com>
2019-01-16 18:02:28 +0330

    gitflow-feature-stash: EditController

--- src/main/java/com/mydvdstore/controller/MovieController.java
+++ src/main/java/com/mydvdstore/controller/MovieController.java
@@ -6,8 +6,10 @@ import com.mydvdstore.service.Impl.MovieServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -23,5 +25,14 @@ public class MovieController {
         model.addAttribute("movies",films);
         return "Movies";
 
+    }
+
+    @GetMapping("movie/{filmId}")
+    public String movieById(@PathVariable int filmId,Model model) throws IOException
+    {
+        Film film = movieService.getMovieById(filmId);
+        model.addAttribute("movie",film);
+
+        return "viewMovie";
     }
 }
